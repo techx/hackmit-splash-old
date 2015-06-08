2bfd69f8c956bbd940c37b8e648c19849f86eada
Jack Serrino <Jserrino@gmail.com>
2015-06-07 23:02:35 -0700

    Add simpleform

--- js/main.js
+++ js/main.js
@@ -31,4 +31,26 @@ $(document).ready(function() {
       $(this).find('.answer').transition('fade down out', 300);
     }
   });
+  $("#interest-form").on('submit', function(){
+    var is_valid_email = function(email) { return (/^.+@.+\..+$/).test(email); };
+    // ^^ Yes, this is easy to break. If you're reading this you're probably smart enough to find a way around it
+    // but there are a thousand other ways to do malicious things so its not worth our time to stop you :)
+    var emailTag = $('#interest-form-email');
+    var email = emailTag.val();
+    if (is_valid_email(email)) {
+      emailTag.val("");
+      $.ajax({
+        dataType: 'jsonp',
+        url: "http://getsimpleform.com/messages/ajax?form_api_token=fe4ccc4ce76bde458f7fbdaf077b89ca",
+        data: {
+          email: email
+        }
+      });
+      $(this).find("label, input").transition('fade out', 100);
+      $(this).find(".thankyou").transition('fade in', 100);
+    } else {
+      // :(
+    }
+    return false;
+  });
 });
