5ee23658205ce4a1623a70549a4d132ee3a5fc4e
Larry Zhang <lzhang124@gmail.com>
2015-06-19 16:16:38 -0600

    final styling fixes

--- js/main.js
+++ js/main.js
@@ -37,8 +37,8 @@ $(document).ready(function() {
           email: email
         }
       });
-      $(this).find('label, input').transition('fade out', 100, function() {
-        $('.thankyou').transition('fade in', 100);
+      $(this).find('label, input').transition('fade out', 500, function() {
+        $('.thankyou').transition('fade in', 500);
       });
       
     } else {
