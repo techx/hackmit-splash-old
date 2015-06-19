574207ed8c1815a620933a184672439df1f10ad4
kimberli <kimberlizhong@gmail.com>
2015-06-18 23:34:24 -0700

    add faq hiding

--- js/main.js
+++ js/main.js
@@ -11,30 +11,62 @@ $(document).ready(function() {
   $tags.click(function(e) {
     var elementName = e.target.id.substr(5);
     $bodytag.animate({
-      scrollTop: $("#"+elementName).offset().top
+      scrollTop: $('#'+elementName).offset().top
     }, 800);
   });
 
-  $("#interest-form").on('submit', function(){
+  hideAnswers();
+
+  $('#interest-form').on('submit', function(){
     var is_valid_email = function(email) { return (/^.+@.+\..+$/).test(email); };
     // ^^ Yes, this is easy to break. If you're reading this you're probably smart enough to find a way around it
     // but there are a thousand other ways to do malicious things so its not worth our time to stop you :)
     var emailTag = $('#interest-form-email');
     var email = emailTag.val();
     if (is_valid_email(email)) {
-      emailTag.val("");
+      emailTag.val('');
       $.ajax({
         dataType: 'jsonp',
-        url: "http://getsimpleform.com/messages/ajax?form_api_token=fe4ccc4ce76bde458f7fbdaf077b89ca",
+        url: 'http://getsimpleform.com/messages/ajax?form_api_token=fe4ccc4ce76bde458f7fbdaf077b89ca',
         data: {
           email: email
         }
       });
-      $(this).find("label, input").transition('fade out', 100);
-      $(this).find(".thankyou").transition('fade in', 100);
+      $(this).find('label, input').transition('fade out', 100);
+      $(this).find('.thankyou').transition('fade in', 100);
     } else {
       // :(
     }
     return false;
   });
+
+  //code for hiding answers
+  $('.question').on('click', function() {
+      if ($(window).width() <= 480) {
+        $question = $(this);
+        $answer = $question.next();
+        $answer.slideToggle(500, function () {
+          $answer.toggleClass('answer-hidden');
+        });
+      }
+    })
+
+  var resizeTimeout;
+  $(window).on('resize', function() {
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(hideAnswers(), 200);
+  })
+
 });
+
+//run on window load and resize
+function hideAnswers() {
+  if ($(window).width() <= 480) {
+    $('.answer').addClass('answer-hidden');
+    $('.question').addClass('question-hidden');
+  }
+  else {
+    $('.answer').removeClass('answer-hidden');
+    $('.question').removeClass('question-hidden');
+  }
+}
