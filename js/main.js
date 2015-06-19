964daf17c96d9589cdb49fb1be92e875ad5e0fe4
Larry Zhang <lzhang124@gmail.com>
2015-06-19 15:14:25 -0600

    small timing fixes

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ $(document).ready(function() {
     clearTimeout(resizeTimeout);
     clearTimeout(resizeNavbarTimeout);
     resizeTimeout = setTimeout(hideAnswers(), 200);
-    resizeNavbarTimeout = setTimeout(recalculateNavbarPosition(), 500);
+    resizeNavbarTimeout = setTimeout(recalculateNavbarPosition(), 200);
   });
 
   /* Following Nav Bar */
@@ -69,7 +69,7 @@ $(document).ready(function() {
   $(window).on('scroll', function() {
     if ($(window).width() >= 786) {
       clearTimeout(scrollTimeout);
-      scrollTimeout = setTimeout(checkAndMoveNavbarPosition(), 400);
+      scrollTimeout = setTimeout(checkAndMoveNavbarPosition(), 000);
     }
   });
 
