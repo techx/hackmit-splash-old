25026f71630f288a0284729da3bf1b6fcade81fa
Anthony Liu <anthonyscottliu@gmail.com>
2015-10-23 20:59:49 -0400

    Removes now-irrelevant sections of the splash page. Changes closing message copy.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     .transition('fade in', 1500);
 
   var $bodytag = $('html, body');
-  var $tags = $('#goto-splash, #goto-blurb, #goto-schedule, #goto-faq, #goto-register, #goto-sponsor');
+  var $tags = $('#goto-splash, #goto-blurb, #goto-faq, #goto-register, #goto-sponsor');
   $tags.click(function(e) {
     var elementName = e.target.id.substr(5);
     if ($(window).width() <= breakWidth) {
